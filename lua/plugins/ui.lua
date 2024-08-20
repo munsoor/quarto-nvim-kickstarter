70b88e7cd91cd4330650e0e70351d7ecb8d338fe
Munsoor Negyal <1674396+munsoor@users.noreply.github.com>
2024-08-20 10:07:04 +0100

    Automate the requirement of magick needed for image.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -345,7 +345,19 @@ return {
       }
     end,
   },
-
+  {
+    -- luarocks.nvim is a Neovim plugin designed to streamline the installation
+    -- of luarocks packages directly within Neovim. It simplifies the process
+    -- of managing Lua dependencies, ensuring a hassle-free experience for
+    -- Neovim users.
+    -- https://github.com/vhyrro/luarocks.nvim
+    "vhyrro/luarocks.nvim",
+    -- this plugin needs to run before anything else
+    priority = 1001,
+    opts = {
+      rocks = { "magick" },
+    },
+  },
   { -- show images in nvim!
     '3rd/image.nvim',
     enabled = true,
