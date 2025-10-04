e2e8ecc398fd2c160d1eafa2a2b6ec621bbfb1a9
Munsoor Negyal <1674396+munsoor@users.noreply.github.com>
2025-10-04 07:17:54 +0100

    chore(neovim): add scroll to centre of the screen

--- lua/config/global.lua
+++ lua/config/global.lua
@@ -112,8 +112,8 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
   end,
 })
 
--- scroll before end of window
-vim.opt.scrolloff = 5
+-- scroll to middle
+vim.opt.scrolloff = 999
 
 -- (don't == 0) replace certain elements with prettier ones
 vim.opt.conceallevel = DefaultConcealLevel
